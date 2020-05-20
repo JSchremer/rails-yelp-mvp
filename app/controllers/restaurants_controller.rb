797dcacbf3cd239716176f9d43b68fc5c2e0d9d1
Juliane Schremer <juliane.schremer@gmail.com>
2020-05-20 17:00:50 +0200

    all done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,26 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(rest_param)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def rest_param
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+
 end
