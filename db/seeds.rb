47e89037b31c81c1f39f2e30a55d8c4e45bab729
Juliane Schremer <juliane.schremer@gmail.com>
2020-05-20 14:15:19 +0200

    added seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "drop it like its hot"
+Restaurant.destroy_all
+
+loca = { name: "loca", address: "Berlin Mitte", category: "italian", phone_number: "63951053"}
+
+otto = { name: "otto", address: "Berlin Prenzlauer Berg", category: "french", phone_number: "0958447"}
+
+robinson = { name: "Mrs Robinson", address: "Berlin Prenzlauer Berg", category: "belgian", phone_number: "57238987"}
+
+ishin = { name: "Ishin", address: "Berlin Mitte", category: "japanese", phone_number: "598930854"}
+
+ernst = { name: "Ernst", address: "Berlin Wedding", category: "chinese", phone_number: "289475890"}
+
+[loca, otto, robinson, ishin, ernst].each do |atts|
+  restaurant = Restaurant.create!(atts)
+  puts "Created #{restaurant.name}"
+end
+puts "done"
